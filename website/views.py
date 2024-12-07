8dc9d1ab175f804861185f89fec6787a1914c06f
Ghinea Ioana-Mara <ioana_mara.ghinea@stud.acs.upb.ro>
2024-12-07 17:11:15 +0200

    Add dummy images. Display images for magazine.

--- website/views.py
+++ website/views.py
@@ -1,37 +1,17 @@
 from flask import Blueprint, render_template, request, flash, jsonify
 from flask_login import login_required, current_user
-from .models import Note
 from . import db
 import json
+import os
 
 views = Blueprint('views', __name__)
 
+MEDIA_ROOT = os.path.join(os.getcwd(), "website/static")
 
 @views.route('/', methods=['GET', 'POST'])
 @login_required
-def home():
-    if request.method == 'POST': 
-        note = request.form.get('note')#Gets the note from the HTML 
-
-        if len(note) < 1:
-            flash('Note is too short!', category='error') 
-        else:
-            new_note = Note(data=note, user_id=current_user.id)  #providing the schema for the note 
-            db.session.add(new_note) #adding the note to the database 
-            db.session.commit()
-            flash('Note added!', category='success')
-
-    return render_template("home.html", user=current_user)
-
-
-@views.route('/delete-note', methods=['POST'])
-def delete_note():  
-    note = json.loads(request.data) # this function expects a JSON from the INDEX.js file 
-    noteId = note['noteId']
-    note = Note.query.get(noteId)
-    if note:
-        if note.user_id == current_user.id:
-            db.session.delete(note)
-            db.session.commit()
-
-    return jsonify({})
+def show_images():
+    image_folder = os.path.join(MEDIA_ROOT, 'images')
+    image_files = [f for f in os.listdir(image_folder) if f.endswith(('jpg', 'png', 'jpeg', 'gif'))]
+    image_paths = [os.path.join('images', img) for img in image_files]
+    return render_template('home.html', images=image_paths, user=current_user)
