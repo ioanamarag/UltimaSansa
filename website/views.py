c431de53aea0461ea72be30d895bc174e84f972f
EsraEmin <esra.emin222@gmail.com>
2024-12-07 17:40:34 +0200

    Afisare poze pe windows

--- website/views.py
+++ website/views.py
@@ -7,13 +7,13 @@ import os
 
 views = Blueprint('views', __name__)
 
-MEDIA_ROOT = os.path.join(os.getcwd(), "website/static")
+MEDIA_ROOT = os.path.join(os.getcwd(), "website","static")
 
 @views.route('/', methods=['GET', 'POST'])
 @login_required
 def show_images():
     image_folder = os.path.join(MEDIA_ROOT, 'images')
     image_files = [f for f in os.listdir(image_folder) if f.endswith(('jpg', 'png', 'jpeg', 'gif'))]
-    image_paths = [os.path.join('images', img) for img in image_files]
+    image_paths = [f"images/{img}" for img in image_files]
     return render_template('home.html', images=image_paths, user=current_user)
 
